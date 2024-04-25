733d64499b003358b4b80c2bc0971144f3dcbc96
Colin Sullivan <colin@colin-sullivan.net>
2024-04-25 10:27:02 -0700

    experimenting with initializing keyboard focus for the juce standalone
    app

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,27 +1,21 @@
 #include "MainComponent.h"
 
 //==============================================================================
-MainComponent::MainComponent()
+MainComponent::MainComponent() : juce::Component(), juce::Timer()
 {
-	webBrowser = std::make_shared<juce::WebBrowserComponent>(new juce::WebBrowserComponent());
+    DBG("MainComponent::MainComponent");
+    setWantsKeyboardFocus(true);
     setSize (600, 400);
-	this->addAndMakeVisible(webBrowser.get());
-	webBrowser.get()->goToURL("http://localhost:3000");
+
+	addAndMakeVisible(webBrowser);
+	webBrowser.goToURL("http://localhost:3000");
+    webBrowser.setWantsKeyboardFocus(true);
+    
+    startTimer(200);
 }
 
 MainComponent::~MainComponent()
 {
-}
-
-//==============================================================================
-void MainComponent::paint (juce::Graphics& g)
-{
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
@@ -31,8 +25,33 @@ void MainComponent::resized()
     // update their positions.
 
 	DBG("resized");
+    
 	int height = getHeight();
 	int width = getWidth();
 
-	webBrowser.get()->setSize(width, height);
+	webBrowser.setSize(width, height);
+    repaint();
+}
+
+void MainComponent::focusGained (juce::Component::FocusChangeType cause) {
+    DBG("focusgained");
+    webBrowser.grabKeyboardFocus();
+}
+
+void MainComponent::visibilityChanged() {
+    DBG("visibilityChanged");
+    
+}
+
+void MainComponent::timerCallback() {
+    DBG("maincmp delayed setup");
+    DBG((hasKeyboardFocus(true) ? "maincmp has keyboard focus" : "maincmp does not have keyboard focus"));
+//    DBG((getParentComponent()->isShowing() ? "parent is showing" : "parent is not showing"));
+    DBG((isShowing() ? "maincmp is visible" : "maincmp is not visible"));
+    if (!hasKeyboardFocus(true) && isShowing())
+    {
+        DBG("maincmp grabbing keyboard focus");
+        grabKeyboardFocus();
+    }
+    stopTimer();
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,12 +2,31 @@
 
 #include <JuceHeader.h>
 
+class WebBrowserWatcher : public juce::ComponentMovementWatcher
+{
+public:
+    WebBrowserWatcher(juce::Component* componentToWatch) : juce::ComponentMovementWatcher(componentToWatch) {};
+    /** This callback happens when the component that is being watched is moved
+            relative to its top-level peer window, or when it is resized. */
+     void componentMovedOrResized (bool wasMoved, bool wasResized) override {};
+
+        /** This callback happens when the component's top-level peer is changed. */
+     void componentPeerChanged() override {};
+
+        /** This callback happens when the component's visibility state changes, possibly due to
+            one of its parents being made visible or invisible.
+        */
+     void componentVisibilityChanged() override {
+         DBG("WebBrowserWatcher::componentVisibilityChanged");
+     };
+};
+
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
+class MainComponent  : public juce::Component, public juce::Timer
 {
 public:
     //==============================================================================
@@ -15,14 +34,19 @@ public:
     ~MainComponent() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
     void resized() override;
+    
+    void focusGained (juce::Component::FocusChangeType cause) override;
+    void visibilityChanged() override;
+    
+    void timerCallback() override;
 
 private:
     //==============================================================================
     // Your private member variables go here...
-
-	std::shared_ptr<juce::WebBrowserComponent> webBrowser;
+//    WebBrowserWatcher mWebBrowserWatcher;
+    juce::WebBrowserComponent webBrowser;
+	
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
